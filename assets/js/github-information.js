836b0eeafa7fea8c1a89ef0eed5703cdff2c888f
Decline-of-Mind <L.h.gotink@gmail.com>
2020-01-31 18:25:10 +0000

    re[po function added, has bug

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -15,6 +15,27 @@ function userInformationHTML(user) {
     </div>`
 }
 
+function repoInformationHTML(repos) {
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">no repos!</div>`
+    }
+
+    var listItemsHTML = repos.map(function(repo) {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`
+    });
+
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                ${listItemsHTML.join("\n")} 
+                </ul>
+            </div>`
+}
+
 
 function fetchGitHubInformation(event) {
 
@@ -35,7 +56,7 @@ function fetchGitHubInformation(event) {
     ).then(
         function(firstResponse, secondResponse) {
             var userData = firstResponse[0];
-            var repoData = secondResponse[0]
+            var repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
             $("#gh-repo-data").html(userInformationHTML(repoData));
         }, function (errorResponse) {
