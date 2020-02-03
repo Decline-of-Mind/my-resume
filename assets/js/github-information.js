89fb66c8d98971c30e2f09614de6a788c0b29b66
Decline-of-Mind <L.h.gotink@gmail.com>
2020-02-03 16:50:22 +0000

    Lingering reposs and usernames no longer displaying on screen when input is empty

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -38,6 +38,10 @@ function repoInformationHTML(repos) {
 
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
+
+
 
     var username = $("#gh-username").val();
     if (!username) {
